53ad36da8654f157b9a7b918982ba081a8d4e175
pratikm0007 <pratikpmondal07@gmail.com>
2022-03-13 22:05:05 +0530

    Push Swap two number file

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace BasicCoreProblem
+{
+    class Program
+    {
+        /// <summary>
+        /// Swap two number.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            int a = 5, b = 10;
+            Console.WriteLine("Before swap a= " + a + "b= " + b);
+            a = a * b;
+            b = a / b;
+            a = a / b;
+            Console.WriteLine("After swap a= " + a + "b= " + b);
+
+
+        }
+    }
+}
