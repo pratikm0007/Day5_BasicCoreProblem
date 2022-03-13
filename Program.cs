19d8e72e3848a2a920002b3a602dbcfec6b6438b
pratikm0007 <pratikpmondal07@gmail.com>
2022-03-14 00:15:21 +0530

    Push EvenOdd file

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace BasicCoreProblem
+{
+    class Program
+    {
+        /// <summary>
+        /// Check Number is Even or Odd.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number ");
+            int Number = Convert.ToInt32(Console.ReadLine());
+
+            if (Number%2 ==0)
+            {
+                Console.WriteLine("Number is Even Number : {0}",+Number);
+            }
+            else
+            {
+                Console.WriteLine("Number is Odd Number : {0}", +Number);
+            }
+           
+        }
+       
+    }
+}
