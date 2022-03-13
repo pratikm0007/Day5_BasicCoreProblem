9a68ddcceb2cff53fc681eedc15e1ca29f2580c5
pratikm0007 <pratikpmondal07@gmail.com>
2022-03-13 22:41:14 +0530

    Push Alphabets check file

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace BasicCoreProblem
+{
+    class Program
+    {
+        /// <summary>
+        /// To check Alphabets enter by user is Vowel or Consonant.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a character");
+
+            string value = Console.ReadLine();
+            char alphabet = Convert.ToChar(value.ToLower());
+
+            switch (alphabet)
+            {
+                case 'a':
+                    Console.WriteLine("Input Character is vowel");
+                    break;
+                case 'e':
+                    Console.WriteLine("Input Character is vowel");
+                    break;
+                case 'i':
+                    Console.WriteLine("Input Character is vowel");
+                    break;
+                case 'o':
+                    Console.WriteLine("Input Character is vowel");
+                    break;
+                case 'u':
+                    Console.WriteLine("Input Character is vowel");
+                    break;
+
+                default:
+                    Console.WriteLine("Input Charcter is consonant");
+                    break;
+            }
+        }
+    }
+}
