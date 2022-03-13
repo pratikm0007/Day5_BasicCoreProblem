a131d45a40381155f562aebc8323b71b29e1035e
pratikm0007 <pratikpmondal07@gmail.com>
2022-03-13 21:31:22 +0530

    Push PowerOf2 file

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace BasicCoreProblem
+{
+    class Program
+    {
+        /// <summary>
+        /// Power of 2 and check it is Leap year or not.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Power value N");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int i;
+            int power2 = 2;
+
+            if (N <= 31)
+            {
+                for (i = 0; i <= N; i++)
+                {
+                    Console.WriteLine("{0}^{1}= {2}", power2, i, (Math.Pow(power2, i)));
+                    double LeapYear = Math.Pow(power2, i);
+
+                    if (((LeapYear % 4 == 0) && (LeapYear % 100 != 0)) || (LeapYear % 400 == 0))
+                    {
+                        Console.WriteLine("{0} is a Leap Year.", LeapYear);
+                    }
+                    else
+                    {
+                        Console.WriteLine("{0} is not a Leapyear.", LeapYear);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("enter valid number");
+            }
+
+        }
+    }
+}
